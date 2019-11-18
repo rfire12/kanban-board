6b59ef2a4e69d6398f4ab3e606616b231087c32f
Raul Paulino <example@example.com>
2019-11-18 14:54:55 -0400

    Board class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Board from './Board/Board';
 
-class App extends Component{
-    render(){
-        return(
-            <div>
-                <h1>My Kanban Board</h1>
-            </div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <Router>
+          <Switch>
+            <Route path="/">
+                <Board />
+            </Route>
+          </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
