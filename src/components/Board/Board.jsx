e4902b8ba33b82c85b7f050e9f6b06a749659aaf
New Name Value <20461484+rfire12@users.noreply.github.com>
2019-11-18 14:54:55 -0400

    Board class

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react-dom";
+import Colunm from "../Colunm/Colunm";
+
+class Board extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Colunm />
+      </div>
+    );
+  }
+}
+
+export default Board;
