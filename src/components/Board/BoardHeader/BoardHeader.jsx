919d2e7b610286d0a992d72cc4879cb3f401dcd6
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2019-12-24 02:04:41 -0400

    Feat: Adds edit board title (#1)

--- src/components/Board/BoardHeader/BoardHeader.jsx
+++ src/components/Board/BoardHeader/BoardHeader.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import styles from "./BoardHeader.scss";
+import Button from "../Button/Button";
 
 const BoardHeader = () => {
   return (
     <>
       <div className={styles.BoardHeader}>
-        <input
-          type="text"
-          name="board-title"
-          value="Store"
-          className={styles.BoardTitle}
-        />
+        <Button />
       </div>
     </>
   );
