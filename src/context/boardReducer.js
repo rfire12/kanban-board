b166f6cb35c91dfcf77d3d346f31f05b5d16627c
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-04-10 21:10:51 -0400

    Formats document

--- src/context/boardReducer.js
+++ src/context/boardReducer.js
@@ -1,11 +1,10 @@
-
 const setLastClickedItem = (lastClickedItem, state) => {
   return { ...state, lastClickedItem: lastClickedItem };
 };
 
 export default (state, action) => {
   switch (action.type) {
-    case 'SET_LAST_CLICKED_ITEM':
+    case "SET_LAST_CLICKED_ITEM":
       return setLastClickedItem(action.payload, state);
     default:
       return state;
