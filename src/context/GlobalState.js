a91544f9f82f28e343b9c3ddd19349b2f8e1754c
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-04-23 19:52:16 -0400

    Feat/Refactor: Changes .gitignore, Deletes dist folder, and formats some files

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -7,7 +7,7 @@ const GlobalState = (props) => {
   const [state, dispatch] = useReducer(boardReducer, { lastClickedItem: null });
 
   const setLastClickedItem = (clickedItem, clickType) => {
-    const lastClickedItem = {item: clickedItem, clickType: clickType}
+    const lastClickedItem = { item: clickedItem, clickType: clickType };
 
     dispatch({
       type: "SET_LAST_CLICKED_ITEM",
