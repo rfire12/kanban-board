786b88a6b5632d9e0bb08cc6c68630cdbbef12e9
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-04-30 16:46:48 -0400

    Refactor: Changes n2n tests structure

--- src/helpers/helpers.js
+++
@@ -1,3 +0,0 @@
-import appRoot from 'app-root-path';
-
-export const getTestPagePath = () => (process.env.TEST_ENV === 'circle_ci' ? `file:${appRoot.path}/dist/index.html` : 'http://127.0.0.1:8080');
