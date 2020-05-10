108ee03e14e198e1cf2e8dd53fccd8eb9e3c216b
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-05-09 21:32:54 -0400

    Feat: Adds Checklist UI

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -5,7 +5,7 @@ import styles from './ProgressBar.scss';
 const ProgressBar = ({ percentage = 0 }) => (
   <div className={styles.wrapper}>
     <div className={styles.percentageNumberWrapper}>
-      <span className={styles.percentageNumber}>25%</span>
+      <span className={styles.percentageNumber}>{`${percentage}%`}</span>
     </div>
     <div className={styles.bar}>
       <div style={{ width: `${percentage}%` }} className={styles.filler} />
